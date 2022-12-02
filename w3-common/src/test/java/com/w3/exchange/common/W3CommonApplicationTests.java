cc017575ad83de3a20d59246de00ad289e3e3fa7
kndopensource <kndopensource@sina.com>
2022-12-02 17:42:03 +0800

    w3-common pom结构调整

--- w3-common/src/test/java/com/w3/exchange/common/W3CommonApplicationTests.java
+++ w3-common/src/test/java/com/w3/exchange/common/W3CommonApplicationTests.java
@@ -1,12 +1,9 @@
 package com.w3.exchange.common;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class W3CommonApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
