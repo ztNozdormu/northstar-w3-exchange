5d7ca9c6b3ebecd830c55bf7ebe0921863c1fe7f
kndopensource <kndopensource@sina.com>
2022-12-05 11:06:53 +0800

    初始化w3-binance模块

---
+++ w3-cex/w3-binance/src/test/java/com/w3/exchange/binance/W3BinanceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.w3.exchange.binance;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class W3BinanceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
