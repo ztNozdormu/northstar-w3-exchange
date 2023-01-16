9c1d51d93ac28c84ea1160b7274ccd2d44488bef
真土 <2307631397@qq.com>
2023-01-16 16:14:57 +0800

    cryptoCompare行情服务商模块初始化

---
+++ w3-third-quotation/w3-third-cryptoCompare/src/test/java/com/w3/exchange/cryptocompare/W3ThirdCryptoCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w3.exchange.cryptocompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3ThirdCryptoCompareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
