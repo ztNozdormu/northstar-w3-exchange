33280a20aa8c7391495db7272a4eaa0ecb58ce7c
真土 <2307631397@qq.com>
2023-01-17 11:19:21 +0800

    全市场币种24小时交易总量排行榜接口封装

--- w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareRank.java
+++ w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareRank.java
@@ -17,7 +17,7 @@ public class CompareRank extends Rank {
     }
 
     /**
-     * 排行榜接口
+     * 币种市值排行榜
      */
     private final String MARKET_CAP_RANK = "/data/top/mktcapfull";
 
@@ -39,9 +39,40 @@ public class CompareRank extends Rank {
      * @return String
      * @see <a href="https://min-api.cryptocompare.com/documentation?key=Toplists&cat=TopTotalMktCapEndpointFull">
      * https://min-api.cryptocompare.com/documentation?key=Toplists&cat=TopTotalMktCapEndpointFull</a>
+     *
      */
     public String marketCapRank(LinkedHashMap<String, Object> parameters) {
         return requestHandler.sendPublicRequest(baseUrl, MARKET_CAP_RANK, parameters, HttpMethod.GET, showLimitUsage);
+    }
+
+    /**
+     * 币种市值排行榜
+     */
+    private final String MARKET_VOL_RANK = "/data/top/totalvolfull";
+
+    /**
+     * marketVolRank 过去24小时内，通过所有市场的总交易量币种排行榜  Market Volume ranking
+     * Toplist by 24H Volume Full Data
+     * Get a number of top coins by their total volume across all markets in the last 24 hours.
+     * Default value is first page (0) and the top 10 coins.
+     * <br><br>
+     * <p>
+     * GET /data/top/totalvolfull
+     * <br>
+     * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
+     * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
+     * @param parameters LinkedHashedMap of String,Object pair
+     *                   where String is the name of the parameter and Object is the value of the parameter
+     *                   <br><br>
+     *                   limit -- int 默认10
+     *                   tsym -- USDT 统计/计算币种
+     * @return String
+     * @see <a href="https://min-api.cryptocompare.com/documentation?key=Toplists&cat=TopTotalVolumeEndpointFull">
+     * https://min-api.cryptocompare.com/documentation?key=Toplists&cat=TopTotalVolumeEndpointFull</a>
+     *
+     */
+    public String marketVolRank(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, MARKET_VOL_RANK, parameters, HttpMethod.GET, showLimitUsage);
     }
 
 }
