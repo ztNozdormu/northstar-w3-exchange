00936522ced61b989638668e1ba2bfdbfe60a6d3
真土 <2307631397@qq.com>
2023-01-17 11:33:05 +0800

    前20交易所总交易量币种排行榜

--- w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareRank.java
+++ w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareRank.java
@@ -46,7 +46,7 @@ public class CompareRank extends Rank {
     }
 
     /**
-     * 币种市值排行榜
+     * 币种在所有市场（交易所）总交易量排行榜
      */
     private final String MARKET_VOL_RANK = "/data/top/totalvolfull";
 
@@ -74,5 +74,33 @@ public class CompareRank extends Rank {
     public String marketVolRank(LinkedHashMap<String, Object> parameters) {
         return requestHandler.sendPublicRequest(baseUrl, MARKET_VOL_RANK, parameters, HttpMethod.GET, showLimitUsage);
     }
+    /**
+     * 币种在前20个市场（交易所）总交易量排行榜
+     */
+    private final String MARKET20_VOL_RANK = "/data/top/totaltoptiervolfull";
 
+    /**
+     * marketVolRank 过去24小时内前20个交易所市场的币种总交易量币种排行  Market Volume ranking
+     * Toplist by 24H Top Tier Volume Full Data
+     * Get a number of top coins by their total top tier volume based on the top 20 markets in the last 24 hours.
+     * Default value is first page (0) and the top 10 coins.
+     * <br><br>
+     * <p>
+     * GET /data/top/totaltoptiervolfull
+     * <br>
+     * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
+     * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
+     * @param parameters LinkedHashedMap of String,Object pair
+     *                   where String is the name of the parameter and Object is the value of the parameter
+     *                   <br><br>
+     *                   limit -- int 默认10
+     *                   tsym -- USDT 统计/计算币种
+     * @return String
+     * @see <a href="https://min-api.cryptocompare.com/documentation?key=Toplists&cat=topTotalTopTierVolumeEndpointFull">
+     * https://min-api.cryptocompare.com/documentation?key=Toplists&cat=topTotalTopTierVolumeEndpointFull</a>
+     *
+     */
+    public String market20VolRank(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, MARKET20_VOL_RANK, parameters, HttpMethod.GET, showLimitUsage);
+    }
 }
