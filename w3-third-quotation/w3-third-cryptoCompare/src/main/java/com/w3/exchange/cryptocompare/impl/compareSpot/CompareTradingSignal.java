0f756cc7b5a9057c76653eaf9dfe5f55ba7f56f1
真土 <2307631397@qq.com>
2023-01-18 10:39:16 +0800

    CryptoCompare最新交易信号接口封装

--- w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareTradingSignal.java
+++ w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareTradingSignal.java
@@ -15,35 +15,34 @@ public class CompareTradingSignal extends TradingSignal {
 
 
     /**
-     * 过去24小时内,币种在所有市场（交易所）总交易量排行榜
+     * 最新交易信号 由智能公司IntoTheBlock提供支持，该公司利用机器学习和高级统计数据为加密资产提取智能信号。
      */
-    private final String MARKET_VOL_RANK = "/data/top/totalvolfull";
+    private final String TRADING_SIGNAL_LATEST = "/data/tradingsignals/intotheblock/latest";
 
     /**
-     * marketVolRank 过去24小时内，通过所有市场的总交易量币种排行榜  Market Volume ranking
-     * Toplist by 24H Volume Full Data
-     * Get a number of top coins by their total volume across all markets in the last 24 hours.
-     * Default value is first page (0) and the top 10 coins.
+     * 由智能公司IntoTheBlock提供支持，该公司利用机器学习和高级统计数据为加密资产提取智能信号。 <br>
+     * tradingSignalLatest 最新交易信号 <br>
+     * Trading Signals Latest <br>
+     * Powered by IntoTheBlock, an intelligence company that leverages machine
+     * learning and advanced statistics to extract intelligent signals for crypto-assets.
      * <br><br>
      * <p>
-     * GET /data/top/totalvolfull
+     * GET /data/tradingsignals/intotheblock/latest
      * <br>
      * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
      * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
      * @param parameters LinkedHashedMap of String,Object pair
      *                   where String is the name of the parameter and Object is the value of the parameter
      *                   <br><br>
-     *                   limit -- int 默认10
-     *                   tsym -- USDT 统计/计算币种
+     *                   fsym -- BTC 交易信号所属币种
      * @return String
      * @see <a href="https://min-api.cryptocompare.com/documentation?key=TradingSignals&cat=tradingSignalsIntoTheBlockLatest">
      * https://min-api.cryptocompare.com/documentation?key=TradingSignals&cat=tradingSignalsIntoTheBlockLatest</a>
      * <br>
      * examples <br>
-     *   https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USDT <br>
-     *   https://min-api.cryptocompare.com/data/top/totalvolfull?tsym=USD&page=2 <br>
+     *   https://min-api.cryptocompare.com/data/tradingsignals/intotheblock/latest?fsym=BTC <br>
      */
-    public String marketVolRank(LinkedHashMap<String, Object> parameters) {
-        return requestHandler.sendPublicRequest(baseUrl, MARKET_VOL_RANK, parameters, HttpMethod.GET, showLimitUsage);
+    public String tradingSignalLatest(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, TRADING_SIGNAL_LATEST, parameters, HttpMethod.GET, showLimitUsage);
     }
 }
