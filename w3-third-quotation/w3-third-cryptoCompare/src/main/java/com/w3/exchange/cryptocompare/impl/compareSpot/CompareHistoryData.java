f62da90fa38b465fb34cf3617634107925500803
真土 <2307631397@qq.com>
2023-01-30 10:38:20 +0800

    从每日历史数据中获取开盘价、收盘价、收盘价、成交量和成交量接口封装

--- w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareHistoryData.java
+++ w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareHistoryData.java
@@ -94,62 +94,43 @@ public class CompareHistoryData extends HistoryData {
     }
 
     /**
-     *  根据token名称获取所有交易对以及交易对所在交易所信息。
+     *  从每日历史数据中获取开盘价、收盘价、收盘价、成交量和成交量
      */
-    private final String PAIR_MAPPING_FSYMS = "/data/v2/pair/mapping/exchange/fsym";
+    private final String HISTORY_DATA_DAY = "/data/v2/histoday";
 
     /**
-     * Exchange From Symbol 根据token名称获取所有交易对以及交易对所在交易所信息
-     * Returns the current pair mapping by exchange symbol.
+     * Daily Pair OHLCV 从每日历史数据中获取开盘价、收盘价、收盘价、成交量和成交量。该值以00:00 GMT时间为准。
+     * 如果e=CCCAGG并且tryConversion=true，则尝试通过BTC或ETH进行转换，以确定最佳可能路径。所使用的转换类型和符号附加在每个历史点上。
+     * 如果想要获得所有可用的历史数据，可以使用limit=2000，
+     * 并使用toTs参数继续回溯。然后，您可以使用:&limit=2000&toTs={接收到的最早时间戳}继续请求批处理。
+     * Get open, high, low, close, volumefrom and volumeto from the daily historical data.
+     * The values are based on 00:00 GMT time. If e=CCCAGG and tryConversion=true, it attempts
+     * conversion through BTC or ETH to determine the best possible path. The conversion type and
+     * symbol used are appended per historical point. If you want to get all the available historical data,
+     * you can use limit=2000 and keep going back in time using the toTs param.
+     * You can then keep requesting batches using: &limit=2000&toTs={the earliest timestamp received}.
      * <br><br>
      * <p>
-     * GET /data/v2/pair/mapping/exchange/fsym
+     * GET /data/v2/histoday
      * <br>
      * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
      * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
      * @param parameters LinkedHashedMap of String,Object pair
      *                   where String is the name of the parameter and Object is the value of the parameter
      *                   <br><br>
-     *                   exchangeFsym -- BTC 查询的token名称
+     *                   fsym -- BTC 查询的币种
+     *                   tsym -- USDT 交易结算币种(一般为稳定币)
+     *                   limit -- 10 数据条数
      *
      * @return String
-     * @see <a href="https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=pairMappingExchangeSymbolEndpoint">
-     * https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=pairMappingExchangeSymbolEndpoint</a>
+     * @see <a href="https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistoday">
+     * https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistoday</a>
      * <br>
      * examples <br>
-     *   https://min-api.cryptocompare.com/data/v2/pair/mapping/exchange/fsym?exchangeFsym=BTC <br>
+     *   https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USDT&limit=10 <br>
      */
-    public String pairMappingFsyms(LinkedHashMap<String, Object> parameters) {
-        return requestHandler.sendPublicRequest(baseUrl, PAIR_MAPPING_FSYMS, parameters, HttpMethod.GET, showLimitUsage);
-    }
-
-    /**
-     *  返回所有计划的pair重映射事件。交易所上币计划？
-     */
-    private final String PAIR_MAPPING_UPDATES = "/data/pair/mapping/planned/updates";
-    /**
-     *
-     * Planned Pair Mapping Updates 返回所有计划的pair重映射事件。交易所上币计划？
-     * Returns all planned pair re-mapping events.
-     * <br><br>
-     * <p>
-     * GET /data/pair/mapping/planned/updates
-     * <br>
-     * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
-     * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
-     * @param parameters LinkedHashedMap of String,Object pair
-     *                   where String is the name of the parameter and Object is the value of the parameter
-     *                   <br><br>
-     *
-     * @return String
-     * @see <a href="https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=plannedPairRemappingEndpoint">
-     * https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=plannedPairRemappingEndpoint</a>
-     * <br>
-     * examples <br>
-     *   https://min-api.cryptocompare.com/data/pair/mapping/planned/updates <br>
-     */
-    public String pairMappingUpdates(LinkedHashMap<String, Object> parameters) {
-        return requestHandler.sendPublicRequest(baseUrl, PAIR_MAPPING_UPDATES, parameters, HttpMethod.GET, showLimitUsage);
+    public String historyDataDay(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, HISTORY_DATA_DAY, parameters, HttpMethod.GET, showLimitUsage);
     }
 
 }
