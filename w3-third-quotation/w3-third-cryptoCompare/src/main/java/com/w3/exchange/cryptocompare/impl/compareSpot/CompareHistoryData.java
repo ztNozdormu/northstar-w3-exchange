4433484da122d377103000ccb183487da3754e4a
真土 <2307631397@qq.com>
2023-01-30 10:31:49 +0800

    获得每小时历史数据的开、高、低、收、量和量接口封装

--- w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareHistoryData.java
+++ w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/CompareHistoryData.java
@@ -42,6 +42,7 @@ public class CompareHistoryData extends HistoryData {
      *                   <br><br>
      *                   fsym -- BTC 查询的币种
      *                   tsym -- USDT 交易结算币种(一般为稳定币)
+     *                   limit -- 10 数据条数
      * @return String
      * @see <a href="https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistominute">
      * https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistominute</a>
@@ -54,34 +55,42 @@ public class CompareHistoryData extends HistoryData {
     }
 
     /**
-     *  通过交易所获取交易所已经上架的币种交易对
+     *  获得每小时历史数据的开、高、低、收、量和量
      */
-    private final String PAIR_MAPPING_EXCHANGE = "/data/v2/pair/mapping/exchange";
+    private final String HISTORY_DATA_HOUR = "/data/v2/histohour";
 
     /**
-     * Mapping - Exchange 按exchange返回当前pair映射。如果e参数中没有传递任何交换，则将获得一个包含所有交换的对象，否则将获得一个包含该特定交换的映射对象的数组
-     * Returns the current pair mapping by exchange. If no exchange is passed in the e parameter,
-     * you will get an object with all the exchanges,
-     * otherwise you get an array with mapping object for that specific exchange
+     * Hourly Pair OHLCV 获得每小时历史数据的开、高、低、收、量和量。如e=CCCAGG并且tryConversion=true，
+     * 则尝试通过BTC或ETH进行转换，以确定最佳可能路径。所使用的转换类型和符号附加在每个历史点上。
+     * 如果希望获得所有可用的历史数据，可以使用limit=2000，
+     * 并使用toTs参数继续回溯。然后，您可以使用:&limit=2000&toTs={接收到的最早时间戳}继续请求批处理。
+     * Get open, high, low, close, volumefrom and volumeto from the hourly historical data.
+     * If e=CCCAGG and tryConversion=true, it attempts conversion through BTC or ETH to determine
+     * the best possible path. The conversion type and symbol used are appended per historical point.
+     * If you want to get all the available historical data, you can use limit=2000 and keep going back
+     * in time using the toTs parameter.
+     * You can then keep requesting batches using: &limit=2000&toTs={the earliest timestamp received}.
      * <br><br>
      * <p>
-     * GET /data/v2/pair/mapping/exchange
+     * GET /data/v2/histohour
      * <br>
      * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
      * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
      * @param parameters LinkedHashedMap of String,Object pair
      *                   where String is the name of the parameter and Object is the value of the parameter
      *                   <br><br>
-     *                   e -- Binance 查询的交易所
+     *                    fsym -- BTC 查询的币种
+     *                    tsym -- USDT 交易结算币种(一般为稳定币)
+     *                    limit -- 10 数据条数
      * @return String
-     * @see <a href="https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=pairMappingExchangeEndpoint">
-     * https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=pairMappingExchangeEndpoint</a>
+     * @see <a href="https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistohour">
+     * https://min-api.cryptocompare.com/documentation?key=Historical&cat=dataHistohour</a>
      * <br>
      * examples <br>
-     *   https://min-api.cryptocompare.com/data/v2/pair/mapping/exchange?e=Binancec <br>
+     *   https://min-api.cryptocompare.com/data/v2/histohour?fsym=BTC&tsym=USDT&limit=10 <br>
      */
-    public String pairMappingExchange(LinkedHashMap<String, Object> parameters) {
-        return requestHandler.sendPublicRequest(baseUrl, PAIR_MAPPING_EXCHANGE, parameters, HttpMethod.GET, showLimitUsage);
+    public String historyDataHour(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, HISTORY_DATA_HOUR, parameters, HttpMethod.GET, showLimitUsage);
     }
 
     /**
