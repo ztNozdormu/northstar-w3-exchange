96ca0a9ad193576b733582d7dab10ecc2dcc5d49
真土 <2307631397@qq.com>
2023-01-30 09:04:11 +0800

    交易所上币事件

--- w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/ComparePairMapping.java
+++ w3-third-quotation/w3-third-cryptoCompare/src/main/java/com/w3/exchange/cryptocompare/impl/compareSpot/ComparePairMapping.java
@@ -53,7 +53,7 @@ public class ComparePairMapping extends PairMapping {
     private final String PAIR_MAPPING_EXCHANGE = "/data/v2/pair/mapping/exchange";
 
     /**
-     * Mapping - Exchange 按exchange返回当前pair映射。如果e参数中没有传递任何交换，则将获得一个包含所有交换的对象，否则将获得一个包含该特定交换的映射对象的数组  Mapping - Exchange
+     * Mapping - Exchange 按exchange返回当前pair映射。如果e参数中没有传递任何交换，则将获得一个包含所有交换的对象，否则将获得一个包含该特定交换的映射对象的数组
      * Returns the current pair mapping by exchange. If no exchange is passed in the e parameter,
      * you will get an object with all the exchanges,
      * otherwise you get an array with mapping object for that specific exchange
@@ -76,6 +76,65 @@ public class ComparePairMapping extends PairMapping {
      */
     public String pairMappingExchange(LinkedHashMap<String, Object> parameters) {
         return requestHandler.sendPublicRequest(baseUrl, PAIR_MAPPING_EXCHANGE, parameters, HttpMethod.GET, showLimitUsage);
+    }
+
+    /**
+     *  根据token名称获取所有交易对以及交易对所在交易所信息。
+     */
+    private final String PAIR_MAPPING_FSYMS = "/data/v2/pair/mapping/exchange/fsym";
+
+    /**
+     * Exchange From Symbol 根据token名称获取所有交易对以及交易对所在交易所信息
+     * Returns the current pair mapping by exchange symbol.
+     * <br><br>
+     * <p>
+     * GET /data/v2/pair/mapping/exchange/fsym
+     * <br>
+     * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
+     * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
+     * @param parameters LinkedHashedMap of String,Object pair
+     *                   where String is the name of the parameter and Object is the value of the parameter
+     *                   <br><br>
+     *                   exchangeFsym -- BTC 查询的token名称
+     *
+     * @return String
+     * @see <a href="https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=pairMappingExchangeSymbolEndpoint">
+     * https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=pairMappingExchangeSymbolEndpoint</a>
+     * <br>
+     * examples <br>
+     *   https://min-api.cryptocompare.com/data/v2/pair/mapping/exchange/fsym?exchangeFsym=BTC <br>
+     */
+    public String pairMappingFsyms(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, PAIR_MAPPING_FSYMS, parameters, HttpMethod.GET, showLimitUsage);
+    }
+
+    /**
+     *  返回所有计划的pair重映射事件。交易所上币计划？
+     */
+    private final String PAIR_MAPPING_UPDATES = "/data/pair/mapping/planned/updates";
+    /**
+     *
+     * Planned Pair Mapping Updates 返回所有计划的pair重映射事件。交易所上币计划？
+     * Returns all planned pair re-mapping events.
+     * <br><br>
+     * <p>
+     * GET /data/pair/mapping/planned/updates
+     * <br>
+     * API KEY in URL - just append ?  or &api_key={your_api_key} the the end of your request url
+     * API KEY in HEADER - add the following header to your request: authorization: Apikey {your_api_key}.
+     * @param parameters LinkedHashedMap of String,Object pair
+     *                   where String is the name of the parameter and Object is the value of the parameter
+     *                   <br><br>
+     *
+     * @return String
+     * @see <a href="https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=plannedPairRemappingEndpoint">
+     * https://min-api.cryptocompare.com/documentation?key=PairMapping&cat=plannedPairRemappingEndpoint</a>
+     * <br>
+     * examples <br>
+     *   https://min-api.cryptocompare.com/data/pair/mapping/planned/updates <br>
+     */
+    public String pairMappingUpdates(LinkedHashMap<String, Object> parameters) {
+        return requestHandler.sendPublicRequest(baseUrl, PAIR_MAPPING_UPDATES, parameters, HttpMethod.GET, showLimitUsage);
     }
 
 }
